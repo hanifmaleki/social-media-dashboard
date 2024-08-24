22882ea8c24ce451c684933a0a642dfb99dd2862
hanif <hanif_1362@yahoo.com>
2024-08-24 11:20:02 +0200

    Add Step-05 Small cards

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app = express()
 
 app.use(express.static(config.paths.outputDir))
 
-app.get('*.html', (req, res) =>{
-    console.log(req.baseUrl)
-    console.log(req.path)
-    console.log('')
-    if (req.url.endsWith('.html')) {
-        res.sendFile(path.join(config.paths.outputDir, 'index.html'))
+app.get('*', (req, res, next) =>{
+    process.stdout.write(req.url + ' ')
+
+    if (req.url.endsWith('.css') || req.url.endsWith('.js')) {
+        next()
+        return
+    } else {
+        if (req.url.endsWith('.html')) {
+            res.sendFile(path.join(config.paths.outputDir, 'index.html'))
+        } 
     }
 }) 
 
